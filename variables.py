cb2c03570d2428da4a7f6dca14259c4b12eceabe
thibaut-poinloup <poinloupthibaut@gmail.com>
2022-11-29 11:46:46 +0100

    Ajout d'un cours sur les variables !
    Création des variable Number et "Chaines de caractère"

---
+++ variables.py
@@ -0,0 +1,11 @@
+# Opérateur d'affection = permet d'injecter une valeur dans une variable !
+my_number_one = 123
+my_number_two = -100
+
+print(my_number_one)
+print(my_number_two)
+
+my_text_one = "Ceci est une chaine de caractère"
+my_text_two = "Ceci est aussi une chaine de caractère"
+
+print(my_text_one)
