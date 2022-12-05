0bc8b4879b3271deea9e4911912b6143e578f763
thibaut-poinloup <poinloupthibaut@gmail.com>
2022-12-05 10:37:35 +0100

    ajout des Nombres à virgules flottantes et de type

--- variables.py
+++ variables.py
@@ -2,10 +2,19 @@
 my_number_one = 123
 my_number_two = -100
 
-print(my_number_one)
+print(type(my_number_one))
 print(my_number_two)
 
 my_text_one = "Ceci est une chaine de caractère"
 my_text_two = "Ceci est aussi une chaine de caractère"
 
 print(my_text_one)
+
+# Nombres à virgules flottantes
+my_number_3 = 3.14
+my_number_4 = -2.71
+my_number_5 = 0.0
+
+print(type(my_number_3))
+print(my_number_4)
+print(my_number_5)
