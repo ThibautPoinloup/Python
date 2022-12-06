503eccc05ce306acee179f00483ad4028cb2b7ff
thibaut-poinloup <poinloupthibaut@gmail.com>
2022-12-06 10:57:01 +0100

    ajout de l'exercice 1 et 2

---
+++ exercices-02/exercice-02-01.py
@@ -0,0 +1,21 @@
+# exo 2.1
+# Affectez :
+# - le nombre 42 à une variable nommée `answer`
+# - le nombre d'or 1,61 à une variable nommée `golden_ratio`
+# - la chaîne de caractères "Salut " suivie de votre nom et prénom à une variable nommée `fullname`
+# - la valeur booléenne vraie si vous aimez Python ou fasse si ce n'est pas le cas, à une variable nommée `love_python`
+# - la valeur nulle `None` à une variable nommée `license_agreement`
+# Affichez ces variables
+
+# réponse 2.1
+answer = 42
+golder_ratio = 1.61
+fullname = "Thibaut Poinloup"
+
+print("Salut" +" "+ fullname)
+
+if(True):
+    print("J'aime Python")
+else:
+    love_python = False
+    print(love_python)   
