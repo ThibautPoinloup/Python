682c207f34ad3255b25ad5de9c84d2cc78aa36b4
thibaut-poinloup <poinloupthibaut@gmail.com>
2022-12-16 11:53:50 +0100

    changement fonction sum avec +

--- exercices-06/exercice-06-10.py
+++ exercices-06/exercice-06-10.py
@@ -5,6 +5,6 @@ my_list = [2.71, 42, 123, 2, 3.14, 1.61]
 
 # réponse 6.10
 
-somme_liste = sum(my_list)
+somme_liste = my_list[0] + my_list[1] + my_list[2] + my_list[3] + my_list[4] + my_list[5] 
 result = somme_liste / 6
 print(result)
