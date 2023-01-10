a987dcd80993308cdee41dd1e82e69acaac97224
thibaut-poinloup <poinloupthibaut@gmail.com>
2023-01-10 18:41:41 +0100

    reponse exercice 6-15

--- exercices-06/exercice-06-15.py
+++ exercices-06/exercice-06-15.py
@@ -5,3 +5,23 @@
 my_list = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit']
 
 # réponse 6.15
+
+for i in my_list:
+    #J'ai fais un print pour pouvoir comparer la valeur la plus grande (11)
+    print(len(i))
+    #Si la taille de mes items sont plus grandes que 10 
+    if len(i) > 10:
+        #tu me donne sa longueur
+        longueur = len(i)
+        #Sa valeur
+        valeur = i
+        # print(longueur, valeur)
+
+        #J'ai refait une bloucle cette fois ci pour comparer la 'valeur' à son index 
+        for index in range(len(my_list)):
+            if my_list[index] == valeur:
+                print(f"L'index du caractère le plus long est {index}, sa longueur est de {longueur}, le mot est '{valeur}'")
+        
+
+    
+    
