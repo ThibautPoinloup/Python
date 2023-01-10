50552dd4f6e033de84162c77be85e6b6c0b6674c
thibaut-poinloup <poinloupthibaut@gmail.com>
2023-01-10 19:38:00 +0100

    reponse exercice 6-16-17

--- exercices-06/exercice-06-16.py
+++ exercices-06/exercice-06-16.py
@@ -11,3 +11,12 @@
 my_list = [2.71, 42, 123, 2, 3.14, 1.61]
 
 # réponse 6.16
+# my_list[0], my_list[1] = my_list[1], my_list[0]
+# my_list[2], my_list[3] = my_list[3], my_list[2]
+# my_list[4], my_list[5] = my_list[5], my_list[4]
+# print(my_list)
+
+for i in range(0, len(my_list), 2):
+    print(i, i + 1)
+    my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+    print(my_list)
--- exercices-06/exercice-06-17.py
+++ exercices-06/exercice-06-17.py
@@ -14,3 +14,27 @@ for _ in range(0, size):
 print(matrix)
 
 # réponse 6.17
+
+for line in range(0, size):
+    if line == 3:
+        line = matrix[2]
+        print(line)
+        for column in range(0, size):
+            if column == 4:
+                column = line[3]
+                print(column)
+
+# La matrice Générée de base
+# [
+#     [64, 63, 79, 50, 85], 
+#     [85, 52, 77, 84, 86], 
+#     [45, 78, 78, 41, 46], 
+#     [75, 58, 66, 62, 74], 
+#     [83, 50, 89, 62, 55]
+# ]
+
+# La ligne 3
+# [45, 78, 78, 41, 46]
+
+# Colonne 4
+# 41
